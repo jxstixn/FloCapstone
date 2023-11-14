4df8f3a198f1955ef7173aa43b25666736f55bf8
jxstixn <93717811+jxstixn@users.noreply.github.com>
2023-11-14 23:28:03 +0100

    Add files via upload

---
+++ src/test/java/com/florian/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
