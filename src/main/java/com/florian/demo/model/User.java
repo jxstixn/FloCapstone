094254920282f9bcb4555b9c7090208a233cc60f
jxstixn <93717811+jxstixn@users.noreply.github.com>
2023-11-14 23:23:51 +0000

    small commit

--- src/main/java/com/florian/demo/model/User.java
+++ src/main/java/com/florian/demo/model/User.java
@@ -22,4 +22,20 @@ public class User {
         this.name = name;
         this.surname = surname;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() { return surname; }
+
+    public void setId(int id) { this.id = id; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setSurname(String surname) { this.surname = surname; }
 }
